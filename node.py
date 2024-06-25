641d40479333760930800376ef43d4c6d6644511
sharkx018 <aryanv018@gmail.com>
2024-06-25 18:48:51 +0530

    Added the wallet class

--- node.py
+++ node.py
@@ -1,13 +1,15 @@
 from blockchain import Blockchain
 from utility.verfication import Verification
+from wallet import Wallet
 
 
 class Node:
 
     def __init__(self):
         # self.id = str(uuid4())
-        self.id = 'mukul'
-        self.blockchain = Blockchain(self.id)
+        # self.id = 'mukul'
+        self.wallet = Wallet()
+        self.blockchain = Blockchain(self.wallet.public_key)
 
     def get_transaction_value(self):
         tx_recipient = input('Enter the recipient of the transaction: ')
@@ -34,7 +36,9 @@ class Node:
             print('1: Add a new transaction value')
             print('2: Mine a new block')
             print('3: Output the blockchain block')
-            print('5: Check transactions validity')
+            print('4: Check transactions validity')
+            print('5: Create wallet')
+            print('6: Load wallet')
             # print('v: Verify the chain')
             print('q: Quit')
 
@@ -45,7 +49,7 @@ class Node:
             if user_choice == '1':
                 tx_data = self.get_transaction_value()
                 recipient, amount = tx_data
-                if self.blockchain.add_transaction(recipient, self.id, amount=amount):
+                if self.blockchain.add_transaction(recipient, self.wallet.public_key, amount=amount):
                     print('Transaction added.')
                 else:
                     print('Invalid transaction')
@@ -54,12 +58,17 @@ class Node:
                 self.blockchain.mine_block()
             elif user_choice == '3':
                 self.print_blockchain_elements()
-            elif user_choice == '5':
-
+            elif user_choice == '4':
                 if Verification.verify_transactions(self.blockchain.get_open_transactions(), self.blockchain.get_balance):
                     print('All transactions are valid.')
                 else:
                     print('There are invalid transactions')
+            elif user_choice == '5':
+                self.wallet.create_keys()
+                self.blockchain.hosting_node_id = self.wallet.public_key
+
+            elif user_choice == '6':
+                pass
             elif user_choice == 'q':
                 waiting_for_input = False
             else:
@@ -68,7 +77,7 @@ class Node:
             if not Verification.verify_chain(self.blockchain.get_chain()):
                 self.print_blockchain_elements()
                 break
-            print('Balance of {}: {:6.2f}'.format(self.id, self.blockchain.get_balance()))
+            print('Balance of {}: {:6.2f}'.format(self.wallet.public_key, self.blockchain.get_balance()))
             # print("get_balance-->>", get_balance(owner))
         else:
             print('User Left!')
