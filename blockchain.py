14b52b2a66bb8f0e2356543602af80192754bb16
sharkx018 <aryanv018@gmail.com>
2024-06-27 01:53:08 +0530

    Added the logic for broadcasting the blocks

--- blockchain.py
+++ blockchain.py
@@ -27,7 +27,6 @@ class Blockchain:
         # Load the blockchain from local storage
         self.load_data()
 
-
     def get_chain(self):
         return self.__chain[:]
 
@@ -172,6 +171,28 @@ class Blockchain:
             return True
         return False
 
+    def add_block(self, block):
+
+        transactions = [Transactions(tx['sender'],
+                                     tx['recipient'],
+                                     tx['signature'],
+                                     tx['amount'])
+                        for tx in block['transactions']]
+
+        proof_is_valid = Verification.valid_proof(transactions[:-1], block['previous_hash'], block['proof'])
+        hashes_match = hash_block(self.get_chain()[-1]) == block['previous_hash']
+
+        if not proof_is_valid or not hashes_match:
+            print('proof_is_valid==>>', proof_is_valid)
+            print('hashes_match==>>', hashes_match)
+            return False
+
+        converted_block = Block(block['index'], block['previous_hash'], transactions, block['proof'],
+                                block['timestamp'])
+        self.__chain.append(converted_block)
+        self.save_data()
+        return True
+
     def mine_block(self):
 
         if self.public_key is None:
@@ -210,11 +231,22 @@ class Blockchain:
             proof,
         )
 
-
-
         self.__chain.append(block)
         self.__open_transactions = []
         self.save_data()
+        for node in self.__peer_nodes:
+            url = 'http://{}/broadcast-block'.format(node)
+
+            converted_block = block.__dict__.copy()
+            converted_block['transactions'] = [tx.__dict__ for tx in converted_block['transactions']]
+            try:
+                response = requests.post(url, json={
+                    'block': converted_block
+                })
+                if response.status_code == 400 or response.status_code == 500:
+                    print('Block declined!, needs resolving')
+            except requests.exceptions.ConnectionError:
+                continue
         return block, "Mining successful!"
 
     def add_peer_node(self, node):
@@ -227,4 +259,3 @@ class Blockchain:
 
     def get_peer_nodes(self):
         return list(self.__peer_nodes)[:]
-
